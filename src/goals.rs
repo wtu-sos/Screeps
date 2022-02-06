ba33a15db43acf1b505c11f95b8ff05f8ecd8256
mowen <595388976@qq.com>
2022-02-06 21:39:34 +0800

    update

--- src/goals.rs
+++ src/goals.rs
@@ -3,7 +3,7 @@ pub fn set_goals() {
     mem.set("home_room", "E3S24");
     mem.set("work_room", vec!["E3S24"]);
 
-    mem.path_set("spawn_goals.E3S24.harvester", 4);
+    mem.path_set("spawn_goals.E3S24.harvester", 6);
     mem.path_set("spawn_goals.E3S24.filler", 0);
     mem.path_set("spawn_goals.E3S24.upgrader", 0);
     mem.path_set("spawn_goals.E3S24.settler", 2);
